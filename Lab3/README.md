1475735dbb41f521fdbcf22486c6ef6b897ecc43
Isaque Araújo <isaquearaujo@dcc.ufrj.br>
2022-05-03 22:47:57 -0300

    Consertando código

---
+++ Lab3/README.md
@@ -0,0 +1,15 @@
+10^5 com 1 thread a aceleração foi de 0.417002
+10^5 com 2 thread a aceleração foi de 0.539661
+10^5 com 4 thread a aceleração foi de 0.000582
+
+10^8 com 1 thread a aceleração foi de 0.929328
+10^8 com 2 thread a aceleração foi de 0.189399
+10^8 com 4 thread a aceleração foi de 0.157219
+
+10^8 com 1 thread a aceleração foi de 0.899449
+10^8 com 2 thread a aceleração foi de 1.324135
+10^8 com 4 thread a aceleração foi de 1.877475
+
+
+
+
--- Lab3/lab3_corrigido.c
+++ Lab3/lab3_corrigido.c
@@ -8,8 +8,8 @@ long int dim;
 float *vetor;
 float menorSeq;
 float maiorSeq;
-float *menorConc;
-float *maiorConc;
+float menorConc;
+float maiorConc;
 long int nthreads;
 
 
@@ -25,43 +25,46 @@ float gerador(float min, float max )
     return min + scale * ( max - min );      /* [min, max] */
 }
 
+typedef struct{
+   long int id;
+   float menorLocal; //identificador do elemento que a thread ira processar
+   float maiorLocal; //dimensao das estruturas de entrada
+} tArgs;
+
 //funcao que as threads executarao
 void *procura(void *arg) {
-   long int id = (long int) arg;
+   tArgs *args = (tArgs*) arg;
+   long int id = (long int) args->id;
    long int tam_bloco = dim/nthreads;
    long int ini = id*tam_bloco;
    long int fim = ini+tam_bloco;
+   /*float *menorLocalThread;
+   menorLocalThread = (float*) malloc(sizeof(float));
+   float *maiorLocalThread;
+   maiorLocalThread = (float*) malloc(sizeof(float)); 
+   *menorLocalThread = (float)args->menorLocal;
+   *maiorLocalThread = (float)args->maiorLocal;*/
    if (id==nthreads-1) fim=dim; //trata sobra
    else fim = ini + tam_bloco;
    for(long int i=ini; i<fim; i++){
-       if(vetor[i] < *menorConc){
-            *menorConc = vetor[i];
+       if(vetor[i] < args->menorLocal){
+            args->menorLocal = vetor[i];
        }
-       if(vetor[i] > *maiorConc){
-            *maiorConc = vetor[i];
-       }
-   }
-   pthread_exit(NULL);
-}
-
-void *procura2(void *arg) {
-   long int id = (long int) arg;
-   for (int i = id; i < dim; i+=nthreads){
-       if(vetor[i] < *menorConc){
-            *menorConc = vetor[i];
-       }
-       if(vetor[i] > *maiorConc){
-            *maiorConc = vetor[i];
+       if(vetor[i] > args->maiorLocal){
+            args->maiorLocal = vetor[i];
        }
    }
-   pthread_exit(NULL);
+   //args->menorLocal = *menorLocalThread;
+   //args->maiorLocal = *maiorLocalThread;
+   pthread_exit((void *) args);
 }
-
 
 //fluxo principal
 int main(int argc, char* argv[]) {
-    pthread_t *tid; //identificadores das threads no sistema    
+    pthread_t *tid; //identificadores das threads no sistema
+    tArgs *args; //identificadores locais das threads e dimensao    
     double inicio, fim, deltaSeq, deltaConc, aceleracao;
+    tArgs *retorno;
 
     if(argc<3) {
       printf("Digite: %s <tamanho do vetor> <numero de threads>\n", argv[0]);
@@ -96,27 +99,36 @@ int main(int argc, char* argv[]) {
     deltaSeq = fim - inicio;
     printf("Tempo Sequencial:%lf\n", deltaSeq);
 
-    menorConc = (float*) malloc(sizeof(float));
-    if(menorConc==NULL) {exit(1);}
-    *menorConc = vetor[0];
-    maiorConc = (float*) malloc(sizeof(float));
-    if(maiorConc==NULL) {exit(1);}
-    *maiorConc = vetor[0];
+    //*menorConc = vetor[0];
+    //*maiorConc = vetor[0];
 
     GET_TIME(inicio);
     //alocacao das estruturas
     tid = (pthread_t*) malloc(sizeof(pthread_t)*nthreads);
     if(tid==NULL) {puts("ERRO--malloc"); return 2;}
+    args = (tArgs*) malloc(sizeof(tArgs)*nthreads);
+    if(args==NULL) {puts("ERRO--malloc"); return 2;}
     //criacao das threads
     for(long int i=0; i<nthreads; i++) {
-        if(pthread_create(tid+i, NULL, procura2, (void*) i)){
+        (args+i)->id = i;
+        (args+i)->menorLocal = vetor[i];
+        (args+i)->maiorLocal = vetor[i];
+        if(pthread_create(tid+i, NULL, procura, (void*) (args+i))){
             puts("ERRO--pthread_create"); return 3;
         }
     } 
 
     //espera pelo termino da threads
     for(long int i=0; i<nthreads; i++) {
-        pthread_join(*(tid+i), NULL);
+        pthread_join(*(tid+i), (void**)&retorno);
+        if(i == 0){
+            menorConc = retorno->menorLocal;
+            maiorConc = retorno->maiorLocal;
+        }
+        if(menorConc > retorno->menorLocal)
+            menorConc = retorno->menorLocal;
+        if(maiorConc < retorno->maiorLocal)
+            maiorConc = retorno->maiorLocal;
     }
     GET_TIME(fim);   
     deltaConc = fim - inicio;
@@ -127,8 +139,8 @@ int main(int argc, char* argv[]) {
     printf("Menor numero sequencial: %.5f\n", menorSeq);
     printf("Maior numero sequencial: %.5f\n", maiorSeq);
 
-    printf("Menor numero concorrente: %.5f\n", *menorConc);
-    printf("Maior numero concorrente: %.5f\n", *maiorConc);
+    printf("Menor numero concorrente: %.5f\n", menorConc);
+    printf("Maior numero concorrente: %.5f\n", maiorConc);
 
     return 0;
 }
